64f550799dba072cdf76ec0838fcff5c7c670105
Santhiago <daniel.santhiago@thrive-wmccann.com>
2020-02-19 23:59:31 -0300

    Mudança no venv

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\daniel.santhiago\onedrive - interpublic\documents\daniel\nestle\gestao_rh\gestao_rh\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
