1d80da9df246a128435ab05153870bb5e7c0bf4e
Santhiago <daniel.santhiago@thrive-wmccann.com>
2020-02-22 17:08:18 -0300

    CRUD de Documentos

--- apps/documentos/views.py
+++ apps/documentos/views.py
@@ -1,3 +1,41 @@
 from django.shortcuts import render
+from django.urls import reverse
 
-# Create your views here.
+from .models import Documento
+from django.views.generic import (
+    CreateView
+)
+
+
+class DocumentoCreate(CreateView):
+    template_name = 'documentos/documento_form.html'
+    models = Documento
+    fields = ['descricao','arquivo']
+
+    def post(self, request, *args, **kwargs):
+        form = self.get_form()
+        form.instance.pertence_id = self.kwargs['funcionario_id']
+
+        if form.is_valid():
+            return self.form_valid(form)
+        else:
+            return self.form_invalid(form)
+
+    # def get_success_url(self):
+    #     return reverse('update_funcionarios', args=[self.kwargs['funcionario_id']])
+
+
+    def get_queryset(self):
+        funcionario_selecionado = self.kwargs['funcionario_id']
+        queryset = Documento.objects.filter(pertence=funcionario_selecionado)
+        return queryset
+
+
+
+    # def form_valid(self, form):
+    #     funcionario = form.save(commit=False)
+    #     username = funcionario.nome.split(' ')[0]+ funcionario.nome.split(' ')[1]
+    #     funcionario.empresa = self.request.user.funcionario.empresa
+    #     funcionario.user = User.objects.create(username=username)
+    #     funcionario.save()
+    #     return super(FuncionarioNovo, self).form_valid(form)
--- gestao_rh/settings.py
+++ gestao_rh/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'apps.documentos',
     'apps.registro_hora_extra',
     'apps.core',
+    'bootstrapform',
 ]
 
 MIDDLEWARE = [
@@ -131,6 +132,12 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
+
 
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'login'
