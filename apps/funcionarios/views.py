e7a2bee1af957fb5ce6d7ae353f7823117038324
Santhiago <daniel.santhiago@thrive-wmccann.com>
2020-02-22 12:40:07 -0300

    CRUD departamentos funcionarios empresa

--- apps/funcionarios/views.py
+++ apps/funcionarios/views.py
@@ -1,5 +1,11 @@
+from django.contrib.auth.models import User
 from django.http import HttpResponse
-from django.views.generic import ListView
+from django.urls import reverse_lazy
+from django.views.generic import (
+    ListView,
+    UpdateView,
+    DeleteView,
+    CreateView)
 from .models import Funcionario
 
 
@@ -10,3 +16,26 @@ class FuncionariosList(ListView):
         empresa_logada = self.request.user.funcionario.empresa
         queryset = Funcionario.objects.filter(empresa=empresa_logada)
         return queryset
+
+
+class FuncionarioEdit(UpdateView):
+    model = Funcionario
+    fields = ['nome','departamentos']
+
+
+class FuncionarioDelete(DeleteView):
+    model = Funcionario
+    success_url = reverse_lazy('list_funcionarios')
+
+
+class FuncionarioNovo(CreateView):
+    model = Funcionario
+    fields = ['nome','departamentos']
+
+    def form_valid(self, form):
+        funcionario = form.save(commit=False)
+        username = funcionario.nome.split(' ')[0]+ funcionario.nome.split(' ')[1]
+        funcionario.empresa = self.request.user.funcionario.empresa
+        funcionario.user = User.objects.create(username=username)
+        funcionario.save()
+        return super(FuncionarioNovo, self).form_valid(form)
